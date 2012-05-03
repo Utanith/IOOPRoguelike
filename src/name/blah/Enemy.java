1c7f73c30eb0d8c36d81211192e4eafc2abfed09
Whit La Torra <wlatorra@hotmail.com>
2012-05-02 20:06:35 -0600

    added items and enemies, player death poorly implemented

---
+++ src/name/blah/Enemy.java
@@ -0,0 +1,51 @@
+package name.blah;
+
+import java.util.Random;
+
+public class Enemy {
+	int x;
+	int y;
+	int health;
+	int damagemin;
+	int damagemax;
+	String id;
+	public Enemy(int x, int y, int health, int damagemin, int damagemax, String id) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.health = health;
+		this.damagemin = damagemin;
+		this.damagemax = damagemax;
+		this.id = id;
+	}
+	
+	public void move(int x, int y, RoguelikeMap map, Player player){
+		Random rand = new Random();
+		if((this.x + 1 == x || this.x - 1 == x) && this.y == y){
+			player.setHealth(player.getHealth() - (rand.nextInt(this.damagemax - this.damagemin) + damagemax));
+			return;
+		}
+		if((this.y + 1 == y || this.y - 1 == y) && this.x == x){
+			player.setHealth(player.getHealth() - (rand.nextInt(this.damagemax - this.damagemin) + damagemax));
+			return;
+		}
+			player.setHealth(player.getHealth() - (rand.nextInt(this.damagemax - this.damagemin) + damagemax));
+		if(this.x > x && map.location(this.x-1, this.y) == "."){
+			this.x--;
+			return;
+		}
+		if(this.x < x && map.location(this.x+1, this.y) == "."){
+			this.x++;
+			return;
+		}
+		if(this.y < y && map.location(this.x, this.y+1) == "."){
+			this.y++;
+			return;
+		}
+		if(this.y > y && map.location(this.x, this.y-1) == "."){
+			this.y--;
+			return;
+		}
+		
+	}
+}
